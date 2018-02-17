80e318c1cf6e7716ba94d9cb9c058f9203b24fcb
sirius note <2hotab2@gmail.com>
2018-02-18 00:17:24 +0200

    Class Server End lesson42 chatroom class server  startServer

--- EvilChatServer/ChatServer/src/Server.java
+++ EvilChatServer/ChatServer/src/Server.java
@@ -55,7 +55,31 @@ public class Server extends JFrame {
         add(new JScrollPane(chatWindow));
         setSize(300,600);
         setVisible(true);
+    }
+    // НАСТРОЙКА И ЗАПУСК СЕРВЕРНОЙ ЧАСТИ
+    // метод запуска которий будет визиваться сразу после старта граф оболочки
+    public void startServer(){
+        //
+        try{
+            // init параметров сервера
+            serverSocket = new ServerSocket(7777, 100); // port and огранич колво подключен
+            // внутрений бесконечний цикл
+            while (true){
+                try{
+                    waitForConnection(); // ждем подключения
+                    setupStreams();  // устанавливаем вход и исход потоки
+                    whileChatting(); // отправляем сообщения и обрабативаем их
+                }
+                catch(EOFException eofException){ //закритие потока
+                    showMessage("\n Сервер оборвал соединение !");
+                } finally {
+                    closeConnection();
+                }
 
-
+            }
+        }
+        catch (IOException ioException){ //ошибки операции ввода вивода
+            ioException.printStackTrace();
+        }
     }
 }
