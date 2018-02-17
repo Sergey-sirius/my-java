aeb525a0d09751f7eae66b1a081e6046f1abc981
sirius note <2hotab2@gmail.com>
2018-02-17 23:33:24 +0200

    Class Server edited

---
+++ EvilChatServer/ChatServer/src/Server.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+// io  ввод вивід
+// net мережа
+// awt ипользуется для рроботи з графічним інтерфейсом
+// awt.event собития
+// javax.swing для робот з графічн інтерф
+
+/**
+ * @version 1
+ * класс chat server
+ */
+// наследуем для отображения окон
+public class Server extends JFrame {
+    // строка для ввода текста
+    private JTextField userInputText;
+    // окно, кто подключился, ip ...
+    private JTextArea chatWindow;
+
+    // потоки ввод вивід
+    private ObjectOutputStream outputStream;
+    private ObjectInputStream inputStream;
+
+    //
+
+}
